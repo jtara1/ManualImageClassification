47170cf9691eddc87c3d9aee2ae435537fc0cc59
James T <jtara@tuta.io>
2017-06-15 16:55:16 -0700

    add so_test for stack overflow question

--- tests/so_test.py
+++ tests/so_test.py
@@ -1,6 +1,22 @@
+from tkinter import *
+
+
 class MyCanvas:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 categories):
+        self.categories = categories
+        self._current_category = categories[0]
+        self._window = None
+
+        self._main()
+
+    def _main(self):
+        master = Tk()
+        self._window = Canvas(master, width=200, height=100)
+        self._window.pack()
+
+        self._assign_each_category_to_hotkey()
+        mainloop()
 
     def _assign_each_category_to_hotkey(self):
         for i in range(len(self.categories)):
@@ -9,3 +25,7 @@ class MyCanvas:
     def _change_category(self, category):
         print(category)
         self._current_category = category
+
+
+if __name__ == "__main__":
+    MyCanvas(categories=['tree', 'monster'])
